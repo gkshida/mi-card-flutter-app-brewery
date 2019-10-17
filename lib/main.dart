edbc3b90475ac63cee6cddf43d2b81a1176f251c
Guilherme Kayo Shida <guilhermeshida@gmail.com>
2019-10-17 18:03:26 -0300

    First commit
    * Added a static analysis;
    * Modified default main;
    * Changed README.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(MiCardApp());
+
+class MiCardApp extends StatelessWidget {
+  const MiCardApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: Container(),
       ),
-    ),
-  );
+    );
+  }
 }
